2f37e14460200aa7a697f088a0c3632d21a73503
jiemu <rebianne03@naver.com>
2025-05-08 14:20:22 +0900

    2025.05.08 stop-line-monitor - 테스트코드 샘플 수정 (mongooseModule connect) * jest 문서 확인 필요 : https://docs.nestjs.com/fundamentals/testing

--- test/station-service.spec.ts
+++ test/station-service.spec.ts
@@ -1,60 +1,60 @@
 import { StationService } from '../src/modules/station/station.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StationRepository } from '../src/modules/station/station.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Station, StationSchema } from '../src/schemas/station.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import testConfig from './test.config';
 
-
-// TODO : connect
-describe('StationService', () => {
+describe('StationService Test With MongooseModule', () => {
     let service: StationService
-    let module: TestingModule
-    let configService: ConfigService
+    const config = testConfig()
 
     beforeEach(async () => {
-        module = await Test.createTestingModule({
+        const module: TestingModule = await Test.createTestingModule({
             imports: [
                 ConfigModule.forRoot({
-                    load: [require('./test.config')],
+                    load: [testConfig],
                     isGlobal: true
                 }),
                 MongooseModule.forRootAsync({
-                    imports: [ConfigModule],
-                    useFactory: (configService: ConfigService) => ({
-                        uri: configService.get('mongodb.uri'),
+                    useFactory: async () => ({
+                        uri: config.mongodb.uri,
                     }),
                     inject: [ConfigService],
                 }),
                 MongooseModule.forFeature([
-                    { name: Station.name, schema: StationSchema }
-                ])
+                    { name: Station.name, schema: StationSchema },
+                ]),
             ],
             providers: [
                 StationService,
+                StationRepository
             ],
         }).compile()
 
         service = module.get<StationService>(StationService)
-        configService = module.get<ConfigService>(ConfigService)
     })
 
-    afterEach(async () => {
-        if (module) {
-            await module.close();
+    it('should be defined', () => {
+        expect(service).toBeDefined()
+    })
+
+    it('should fetch stations from MongoDB', async () => {
+        try {
+            const stations = await service.getStationListForBatch(5)
+            console.log('results : ', stations)
+
+            expect(stations).toBeDefined()
+            expect(Array.isArray(stations)).toBe(true)
+
+            if (stations.length > 0) {
+                expect(stations[0].stationId).toBeDefined();
+            }
+
+        } catch (error) {
+            console.error('error : ', error)
         }
     })
 
-    it('should fetch real stations from database', async () => {
-        const stations = await service.getStationListForBatch(500)
-        console.log('Fetched stations:', JSON.stringify(stations, null, 2))
-
-        expect(stations).toBeDefined()
-        expect(Array.isArray(stations)).toBe(true)
-
-        // 데이터 체크
-        if (stations.length > 0) {
-            console.log('First station:', stations[0])
-            expect(stations[0].stationId).toBeDefined()
-        }
-    })
 })
