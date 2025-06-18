3569c78b224f8c19753935eb3259934e39e93ba2
jiemu <rebianne03@naver.com>
2025-06-18 13:06:32 +0900

    2025.06.18 stop-line-monitor

--- test/test.config.ts
+++ test/test.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
     mongodb: {
-        uri: 'mongodb+srv://rebianne03:N0ednAMhsi0hc8F8@stop-line-monitor.tnkxsct.mongodb.net/stop-line-monitor?retryWrites=true&w=majority&appName=stop-line-monitor'
+        uri: ''
     },
     publicApi: {
         stationInfoUrl: '',
