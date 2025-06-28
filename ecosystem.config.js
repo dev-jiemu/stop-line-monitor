5e205dabeaf7bee3e013366d0e6f6c8d83464a35
jiemu <rebianne03@naver.com>
2025-06-29 01:05:49 +0900

    2025.06.29 stop-line-monitor - ecosystem tz add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  apps: [{
-    name: 'stop-line-monitor',
-    script: './dist/src/main.js',
-    cwd: '/home/ec2-user/deployment/stop-line-monitor',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-}
+    apps: [{
+        name: 'stop-line-monitor',
+        script: './dist/src/main.js',
+        cwd: '/home/ec2-user/deployment/stop-line-monitor',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            TZ: 'Asia/Seoul',
+            NODE_ENV: 'production',
+        },
+    }],
+};
